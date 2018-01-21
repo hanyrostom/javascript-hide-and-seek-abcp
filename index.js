6102dfe785d485406dc6d42bf4dec771e801ecae
Hany Rostom <hanyrostom@gmail.com>
2018-01-21 12:37:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
-  
+
   var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
 
   for (let i = 0; i < lis.length; i++) {
