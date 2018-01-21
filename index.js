0f00fad2af3342d29070cd1c36447391e6376e6a
Hany Rostom <hanyrostom@gmail.com>
2018-01-21 12:24:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function deepestChild() {
 
 function increaseRankBy(n) {
   var integer = parseInt(n);
-  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
 
   for (let i = 0; i < lis.length; i++) {
   lis[i].innerHTML = parseInt(lis[i]) + n;
