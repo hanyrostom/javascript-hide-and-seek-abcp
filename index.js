243160797f516feaab35f83d7f277885e9ed6546
Hany Rostom <hanyrostom@gmail.com>
2018-01-21 11:57:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelectorAll('.target')
 }
